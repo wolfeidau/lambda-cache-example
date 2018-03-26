4976fe7cebcfb0855e6d60bea144ce1321c6b819
wolfeidau <mark.wolfe@versent.com.au>
2018-03-27 10:07:17 +1100

    fix(AWS) Updated session create to use new syntax.

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -17,31 +17,31 @@ var (
 	errNameNotProvided  = errors.New("no name was provided in the HTTP body")
 	errConfigLoadFailed = errors.New("unable to load configuration")
 
-	cache = ssmcache.New(session.New())
+	cache = ssmcache.New(session.Must(session.NewSession()))
 )
 
 // Handler is your Lambda function handler
 // It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
 // However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func Handler(request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 
 	// stdout and stderr are sent to AWS CloudWatch Logs
 	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
 
 	// If no name is provided in the HTTP request body, return an error
 	if len(request.Body) < 1 {
-		return events.APIGatewayProxyResponse{}, errNameNotProvided
+		return nil, errNameNotProvided
 	}
 
 	val, err := cache.GetKey(defaultSSMKey)
 	// if we failed to load configuration return an error
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, errConfigLoadFailed
+		return nil, errConfigLoadFailed
 	}
 
 	log.Printf("key: %s value: %s", defaultSSMKey, val)
 
-	return events.APIGatewayProxyResponse{
+	return &events.APIGatewayProxyResponse{
 		Body:       "Hello " + request.Body,
 		StatusCode: 200,
 	}, nil
